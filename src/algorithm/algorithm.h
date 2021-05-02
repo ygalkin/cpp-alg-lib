502bc62dc90d7223f27245db9f2d3cc703b10c31
ygalkin <y.galkin@gmail.com>
2021-05-02 10:00:20 -0400

    update

--- src/algorithm/algorithm.h
+++ src/algorithm/algorithm.h
@@ -1,7 +1,49 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
+#include <string>
+
 namespace algorithm {
+
+    size_t naive_string_search(const std::string& text, const std::string& pattern) {
+        if (pattern.empty()) {
+            return 0;
+        }
+
+        if (text.size() < pattern.size()) {
+            return std::string::npos;
+        }
+
+        for (size_t i = 0; i <= text.size() - pattern.size(); ++i) {
+            if (text[i] == pattern[0]) {
+                size_t j = 1;
+                for (; j < pattern.size(); ++j) {
+                    if (text[i + j] != pattern[j]) {
+                        break;
+                    }
+                }
+
+                if (j == pattern.size()) {
+                    return i;
+                }
+            }
+        }
+
+        return std::string::npos;
+    }
+
+    // knuth_morris_pratt_string_search
+    size_t kmp_string_search(const std::string& text, const std::string& pattern) {
+        if (pattern.empty()) {
+            return 0;
+        }
+
+        if (text.size() < pattern.size()) {
+            return std::string::npos;
+        }
+
+        return std::string::npos;
+    }
 
 }
 
--- src/algorithm/test.h
+++ src/algorithm/test.h
@@ -5,7 +5,10 @@
 
 namespace algorithm {
 
-    TEST_CASE("", "[algorithm]") {
+    TEST_CASE("Naive string search", "[algorithm]") {
+        REQUIRE(algorithm::naive_string_search("al", "algo") == std::string::npos);
+        REQUIRE(algorithm::naive_string_search("abcd", "e") == std::string::npos);
+        REQUIRE(algorithm::naive_string_search("pattern search algorithm example", "algo") == 15);
     }
 }
 
