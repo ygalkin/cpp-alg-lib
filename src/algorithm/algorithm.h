1d7a33fd67c7401b93fe786e6f2606b6059d25aa
ygalkin <y.galkin@gmail.com>
2021-05-02 09:14:14 -0400

    update

--- src/algorithm/algorithm.h
+++ src/algorithm/algorithm.h
@@ -1,36 +1,8 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
-#include <cctype>
-#include <unordered_map>
-
 namespace algorithm {
 
-    unsigned int roman_to_int(const std::string& s) {
-        const std::unordered_map<char, int> lookup_tbl = {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},
-            {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-
-        unsigned int n{ 0 };
-        int i{ 0 };
-        auto prev{ 0 };
-        auto curr{ 0 };
-        const size_t last = s.size() - 1;
-
-        for (i = last; i >= 0; --i) {
-            curr = lookup_tbl.at(s[i]);
-            n = (i < last) && (curr < prev) ? n - curr : n + curr;
-            prev = curr;
-        }
-
-        return n;
-    }
-
-    // user defined literal
-    unsigned int operator"" _roman(const char* str, size_t /*size*/) {
-        return roman_to_int({ str });
-    }
 }
 
 #endif
--- src/algorithm/test.h
+++ src/algorithm/test.h
@@ -5,19 +5,7 @@
 
 namespace algorithm {
 
-    TEST_CASE("roman number", "[algorithm]") {
-        REQUIRE(algorithm::roman_to_int("I") == 1);
-        REQUIRE(algorithm::roman_to_int("X") == 10);
-        REQUIRE(algorithm::roman_to_int("MMXXI") == 2021);
-
-        // user defined literal
-        REQUIRE("MM"_roman == 2000);
-        REQUIRE("MMMM"_roman == 4000);        
-
-        // additive notation and subtractive notation
-        REQUIRE("XXXXXX"_roman == "LX"_roman);
-        REQUIRE("IIIIII"_roman == "VI"_roman);
-        REQUIRE("IIII"_roman == "IV"_roman);
+    TEST_CASE("", "[algorithm]") {
     }
 }
 
