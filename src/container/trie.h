208e271cb3b860b7c7355cb01cca56afc75c3d44
ygalkin <y.galkin@gmail.com>
2021-11-03 19:31:48 -0700

    update

--- src/container/trie.h
+++ src/container/trie.h
@@ -2,7 +2,6 @@
 #define _CONTAINER_TRIE_
 
 #include <unordered_map>
-#include <memory>
 #include <string_view>
 
 // Trie (prefix tree). 
@@ -16,27 +15,29 @@ namespace container {
     private:
         struct trie_node {
             trie_node() : _is_complete_word{ false } {};
-            trie_node(const trie_node& other) = delete;
-            trie_node(trie_node&& other) = delete;
-            trie_node& operator = (const trie_node& other) = delete;
-            trie_node& operator = (trie_node&& other) = delete;
+            trie_node(const trie_node& other) = default;
+            trie_node(trie_node&& other) = default;
+            trie_node& operator = (const trie_node& other) = default;
+            trie_node& operator = (trie_node&& other) = default;
             ~trie_node() = default;
 
-            std::unordered_map<char, std::unique_ptr<trie_node>> _children;
+            std::unordered_map<char, trie_node> _children;
             bool _is_complete_word;
         };
 
-        std::unique_ptr<trie_node> _root;
+        trie_node _root;
 
         const trie_node* _search(const std::string_view& word) const {
-            auto node = _root.get();
+            const auto* node = &_root;
 
             for (const auto& ch : word) {
-                if (node->_children.count(ch) == 0) {
+                const auto iter = node->_children.find(ch);
+
+                if (iter == std::cend(node->_children)) {
                     return nullptr;
                 }
 
-                node = node->_children[ch].get();
+                node = &iter->second;
             }
 
             return node;
@@ -53,12 +54,12 @@ namespace container {
             }
 
             for (const auto& child : node->_children) {
-                _for_each(child.second.get(), f, std::string{ word } + child.first);
+                _for_each(&child.second, f, std::string{ word } + child.first); // TODO
             }
         }
 
     public:
-        trie() { _root = std::make_unique<trie_node>(); }
+        trie() = default;
         trie(const trie& other) = delete;
         trie(trie&& other) = delete;
         trie& operator = (const trie& other) = delete;
@@ -67,14 +68,14 @@ namespace container {
 
         // insert a word into the trie.
         void insert(const std::string_view& word) {
-            auto node = _root.get();
+            auto node = &_root;
 
             for (const auto& ch : word) {
                 if (node->_children.count(ch) == 0) {
-                    node->_children.emplace(ch, std::make_unique<trie_node>());
+                    node->_children.emplace(ch, trie_node{});
                 }
 
-                node = node->_children[ch].get();
+                node = &node->_children.at(ch);
             }
 
             node->_is_complete_word = true;
@@ -94,14 +95,14 @@ namespace container {
 
         // true if trie is empty
         bool empty() const noexcept {
-            return _root->_children.empty();
+            return _root._children.empty();
         }
 
         void clear() noexcept {
-            return _root->_children.clear();
+            return _root._children.clear();
         }
 
-        void for_each(const std::string_view& prefix, std::function<void(const std::string_view&)> f) const noexcept {
+        void for_each(const std::string_view& prefix, std::function<void(const std::string_view&)> f) const noexcept { // TODO
             const auto node = _search(prefix);
             if (node == nullptr) {
                 return;
